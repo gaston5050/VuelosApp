6b70c71d5b2665fd14ef1fdb68ae6c541681f019
gaston5050 <gaston14101985@gmail.com>
2026-07-21 23:13:06 -0300

    Sigo trabajando con el DAO

--- app/src/main/java/com/example/vuelosapp/data/clases/AirportDao.kt
+++ app/src/main/java/com/example/vuelosapp/data/clases/AirportDao.kt
@@ -19,6 +19,16 @@ interface AirportDao {
         @Delete
         suspend fun delete(airport: Airport)
 
+        //Query para que devuelva nombre de aeropuerto/ codigo% IATA  a medida que se escribe en el imput
+        @Query("SELECT * FROM airport WHERE name like  :input OR iata_code  like :input")
+        fun getAirports(input: String): Flow<List<Airport>>
+
+        @Query ("SELECT * FROM airport WHERE iata_code != :input ")
+        fun getPossibleDestinations(input: String): Flow<List<Airport>>
+
+        @Query ("Select * from favorite")
+        fun getFavorites():Flow<List<Favorite>>
+
      //   @Query("SELECT * FROM ITEMS WHERE ID = :id")
      //   fun getItem(id: Int): Flow<Item>
 
