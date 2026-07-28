fa4c256c35ed84ba826ed129f5314ddb33d9176e
gaston5050 <gaston14101985@gmail.com>
2026-07-27 23:44:23 -0300

    Trabajando en RoomDatabase

--- app/src/main/java/com/example/vuelosapp/data/AirportDatabase.kt
+++ app/src/main/java/com/example/vuelosapp/data/AirportDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.vuelosapp.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
@@ -9,5 +11,25 @@ abstract class AirportDatabase: RoomDatabase() {
 
     //
     abstract fun AirportDao(): AirportDao
+
+
+    companion object {
+        @Volatile
+        private var Instance: AirportDatabase? = null
+
+        fun getDatabase(context: Context): AirportDatabase{
+            return Instance ?: synchronized(this){
+                Room.databaseBuilder(context, AirportDatabase::class.java, "vuelo_database")
+                    .createFromAsset("database/flight_search.db" )
+                    .build()
+                    .also { Instance= it }
+            }
+        }
+
+
+
+
+
+    }
 
 }
